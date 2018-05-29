97f595a4e0d91fbc1a5077e3416df7395de4316b
Giuseppe <gdaristotile@gmail.com>
2018-05-29 11:06:40 -0400

    utf8

--- wp-config.php
+++ wp-config.php
@@ -62,7 +62,7 @@ define('DB_PASSWORD', 'GZHkVjr4');
 define('DB_HOST', 'mysql.bookwyrmspod.com');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
